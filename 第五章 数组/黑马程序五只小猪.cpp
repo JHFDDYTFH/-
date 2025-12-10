43c783fcd509560c2c2c132e5817d3a025363c1a
JHFDDYTFH <3635086256@qq.com>
2025-12-10 10:07:39 +0800

    简单排序（和0比较）

---
+++ 第五章 数组/黑马程序五只小猪.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace  std;
+int main()
+{
+    int arr[5]={300,350,200,400,250};
+    int max=0;//认定一个最大值为0
+    for (int i=0;i<5;i++)
+    {
+        if(arr[i]>max)
+        max=arr[i];
+    }
+    cout<<"max="<<max<<endl;
+    return 0;
+}
